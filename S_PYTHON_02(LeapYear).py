ae1737064390471694ca2d6e4ff9059e236c723c
abdallahyounis01 <128917412+abdallahyounis01@users.noreply.github.com>
2023-05-04 02:12:17 +0300

    Update S_PYTHON_02(LeapYear).py

--- S_PYTHON_02(LeapYear).py
+++ S_PYTHON_02(LeapYear).py
@@ -1,7 +1,7 @@
 def leap_year():
 
     year = int(input("Please Enter The Year :"))
-    if year % 4 == 0 and (year % 100 != 0 and year % 400 == 0):
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
         print("{} Is A Leap Year".format(year))
 
 
